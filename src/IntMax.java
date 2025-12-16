da60feee3dbfa976fc80eefe653218d21f7f1c17
ThulluriSrivalliUma <thullurisrivalli@gmail.com>
2025-12-16 11:16:15 +0530

    Checking Maximum of 3 Float objects using CompareTo method

--- src/IntMax.java
+++ src/IntMax.java
@@ -9,4 +9,14 @@ public class IntMax {
         }
         return max;
     }
+    public static Float FindMax(Float a, Float b, Float c){
+        Float max=a;
+        if(b.compareTo(max)>0){
+            max=b;
+        }
+        if(c.compareTo(max)>0){
+            max=c;
+        }
+        return max;
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -5,12 +5,12 @@ public class Main {
         System.out.println("Find Maximum Problem using Generics");
         Scanner sc=new Scanner(System.in);
         System.out.println("Enter the first number: ");
-        Integer a= sc.nextInt();
+        Float a= sc.nextFloat();
         System.out.println("Enter the Second number: ");
-        Integer b= sc.nextInt();
+        Float b= sc.nextFloat();
         System.out.println("Enter the Third number: ");
-        Integer c= sc.nextInt();
-        Integer out=IntMax.FindMax(a,b,c);
+        Float c= sc.nextFloat();
+        Float out=IntMax.FindMax(a,b,c);
         System.out.println(out+" is the maximum number among "+a+","+b+","+c);
     }
 }
