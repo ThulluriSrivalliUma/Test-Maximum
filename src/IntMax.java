3791ee17386faac291d8746154946082bdd3d5f8
ThulluriSrivalliUma <thullurisrivalli@gmail.com>
2025-12-16 11:10:04 +0530

    Checking Maximum of 3 using CompareTo method

---
+++ src/IntMax.java
@@ -0,0 +1,12 @@
+public class IntMax {
+    public static Integer FindMax(Integer a, Integer b, Integer c){
+        Integer max=a;
+        if(b.compareTo(max)>0){
+            max=b;
+        }
+        if(c.compareTo(max)>0){
+            max=c;
+        }
+        return max;
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,14 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args){
         System.out.println("Find Maximum Problem using Generics");
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the first number: ");
+        Integer a= sc.nextInt();
+        System.out.println("Enter the Second number: ");
+        Integer b= sc.nextInt();
+        System.out.println("Enter the Third number: ");
+        Integer c= sc.nextInt();
+        Integer out=IntMax.FindMax(a,b,c);
+        System.out.println(out+" is the maximum number among "+a+","+b+","+c);
     }
 }
