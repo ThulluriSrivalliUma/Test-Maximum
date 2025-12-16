322117a5f8d34fe3c41ac6c7f903d845ea9bdbd1
ThulluriSrivalliUma <thullurisrivalli@gmail.com>
2025-12-16 11:42:14 +0530

    Checking Maximum of 3 Generic objects using CompareTo method

--- src/Maximum.java
+++ src/Maximum.java
@@ -1,6 +1,6 @@
 public class Maximum {
-    public static Integer FindMax(Integer a, Integer b, Integer c){
-        Integer max=a;
+    public static <T extends Comparable<T>> T FindMax(T a, T b, T c){
+        T max=a;
         if(b.compareTo(max)>0){
             max=b;
         }
@@ -9,24 +9,5 @@ public class Maximum {
         }
         return max;
     }
-    public static Float FindMax(Float a, Float b, Float c){
-        Float max=a;
-        if(b.compareTo(max)>0){
-            max=b;
-        }
-        if(c.compareTo(max)>0){
-            max=c;
-        }
-        return max;
-    }
-    public static String FindMax(String a, String b, String c){
-        String max=a;
-        if(b.compareTo(max)>0){
-            max=b;
-        }
-        if(c.compareTo(max)>0){
-            max=c;
-        }
-        return max;
-    }
+
 }
